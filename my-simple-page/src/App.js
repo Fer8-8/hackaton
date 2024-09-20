7db693978335fda308ff7c3c5672f5a5d2780b3b
Fer8-8 <fernandonajerarascon@gmail.com>
2024-09-19 19:20:06 -0500

    Starting

--- my-simple-page/src/App.js
+++ my-simple-page/src/App.js
@@ -5,16 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My Simple React Page</h1>
-        <p>This is a simple webpage built with React!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ingrese informacion sobre su negocio</h1>
+
+        <textarea></textarea>
+
+        <div>
+          <button>Ingresar</button>
+        </div>
       </header>
     </div>
   );
